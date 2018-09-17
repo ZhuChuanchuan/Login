90a4a0589f23119e92a2da5b8e4011115b08bfbe
ZHCC <zhuchuanchuan0904@gmail.com>
2018-09-17 08:47:25 +0800

    filter拦截

---
+++ src/com/zhcc/servlet/AuthorityFilter.java
@@ -0,0 +1,46 @@
+package com.zhcc.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Create by ZHCC on 2018/9/17
+ */
+@WebFilter(filterName = "AuthorityFilter",urlPatterns = {"/*"},initParams = {
+        @WebInitParam(name="encoding",value="GBK"),
+        @WebInitParam(name="loginPage",value="/index.jsp")
+})
+public class AuthorityFilter implements Filter {
+
+    private FilterConfig config;
+    public void destroy() {
+        this.config=null;
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        String encoding = config.getInitParameter("encoding");
+        String loginPage = config.getInitParameter("loginPage");
+
+        req.setCharacterEncoding(encoding);
+        HttpServletRequest requ=(HttpServletRequest)req;
+        HttpSession session = requ.getSession(true);
+        String requestPath=requ.getServletPath();
+        if (session.getAttribute("user") == null && !requestPath.endsWith(loginPage)) {
+            //forward到登录界面
+            req.setAttribute("tip", "您还没登录");
+            req.getRequestDispatcher(loginPage).forward(req,resp);
+        }else{//放行请求
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+        this.config=config;
+    }
+
+}
---
+++ src/com/zhcc/servlet/LogFilter.java
@@ -0,0 +1,41 @@
+package com.zhcc.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Create by ZHCC on 2018/9/17
+ */
+@WebFilter(filterName = "LogFilter",urlPatterns = {"/*"})  //拦截所有请求
+public class LogFilter implements Filter {
+
+    private FilterConfig config;
+    public void destroy() {
+        this.config=null;
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        //请求预处理
+        ServletContext context=this.config.getServletContext();
+        long before=System.currentTimeMillis();
+        System.out.println("开始过滤...");
+        HttpServletRequest hrequest=(HttpServletRequest)req;
+        //输出提示信息
+        System.out.println("Filter已拦截到请求地址：" + hrequest.getServletPath());
+
+        //请求依然放行
+        chain.doFilter(req,resp);
+
+        //服务器响应后处理
+        long after=System.currentTimeMillis();
+        System.out.println("过滤结束");
+        System.out.println("请求被定位到"+hrequest.getRequestURI()+" 所花时间"+(after-before));
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+        this.config=config;
+    }
+
+}
